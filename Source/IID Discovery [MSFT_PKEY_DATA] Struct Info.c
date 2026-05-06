2fbab40c2904477ae3a0afdb93ef1c4d9603f096
BlueOnBLack <114880512+BlueOnBLack@users.noreply.github.com>
2026-05-06 21:44:23 +0300

    Add files via upload

--- Source/IID Discovery [MSFT_PKEY_DATA] Struct Info.c
+++ Source/IID Discovery [MSFT_PKEY_DATA] Struct Info.c
@@ -3,8 +3,8 @@
 
 // Size: 0x58 (88 bytes) // v12
 typedef struct _MSFT_PKEY_DATA {
-	uint64_t Reserved;          // 0x00
-	uint8_t  Signature[16];     // 0x08
+	uint64_t Header;            // 0x00
+	Guid     Algorithm;         // 0x08
 	uint32_t GroupID;           // 0x18
 	uint32_t ChannelID;         // 0x1C
 	uint32_t Serial;            // 0x20
